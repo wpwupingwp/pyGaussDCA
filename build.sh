97a42d6e557c8a876513937693a9c09bd9181bc7
DL <wpwupingwp@outlook.com>
2026-04-03 18:18:49 +0800

    linux python 3.10-3.14 ok

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 rm -rf src/gaussdca/*.cpp
 rm -rf build/*
 rm -rf dist/gaussdca
-rm -rf dist/pygaussdca-0.1.0.dist-info
-rm -rf dist/pygaussdca-0.1.0-cp312-cp312-linux_x86_64.whl
+rm -rf dist/pygaussdca-*.dist-info
 #uv run python3 setup.py build
 #uv run python3 setup.py build_ext
 uv run python3 setup.py bdist_wheel
