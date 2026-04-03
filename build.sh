012022ce1d74b97f321fa7c2918ad6d190e656f2
DL <wpwupingwp@outlook.com>
2026-04-03 17:00:10 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-uv run python3 setup.py build
-uv run python3 setup.py build_ext
+rm -rf build/*
+rm -rf dist/gaussdca
+rm -rf dist/pygaussdca-0.1.0.dist-info
+rm -rf dist/pygaussdca-0.1.0-cp312-cp312-linux_x86_64.whl
+#uv run python3 setup.py build
+#uv run python3 setup.py build_ext
 uv run python3 setup.py bdist_wheel
