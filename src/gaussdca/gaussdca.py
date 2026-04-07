2b46d1f02c04250207384286758094e65a1004b3
Ping Wu <wpwupingwp@outlook.com>
2026-04-07 16:42:51 +0800

    try to add log

--- src/gaussdca/gaussdca.py
+++ src/gaussdca/gaussdca.py
@@ -1,11 +1,88 @@
+import argparse
 from pathlib import Path
-from sys import argv
+from typing import Iterable
+from sys import stdout, argv
 
 import numpy as np
 from scipy import linalg
+from loguru import logger as log
 
 from . import _gdca
 from . import _load_data
+
+NAME = 'GaussDCApy'
+VERSION = '1.0.0'
+log.remove()
+fmt = ('<green>{time:MM-DD HH:mm:ss}</green> | '
+       '<level>{level: <8}</level> | '
+       # todo: remove in release version
+       # '<cyan>{name}</cyan>:'
+       # '<cyan>{function}</cyan>:'
+       # '<cyan>{line}</cyan> - '
+       '<level>{message}</level>')
+log.add(stdout, colorize=True, format=fmt, level='INFO', filter=NAME,
+        backtrace=True, enqueue=True)
+log.info(f'Starting {NAME.upper()} v{VERSION}')
+
+
+def parse_fasta(fasta_file: Path) -> Iterable[tuple[str, str]]:
+    """
+    Import from treebarcode.
+    Upper letter
+    Return tuple(title: str, sequence: str)
+    """
+    with open(fasta_file, 'r') as f:
+        record = []
+        title = ''
+        for line_raw in f:
+            line = line_raw.strip()
+            if line.startswith('>'):
+                if len(record) != 0:
+                    join_str = ''.join(record)
+                    if len(join_str.strip()) != 0:
+                        yield title, join_str.upper()
+                    else:
+                        log.warning(f'Found empty sequence {title}')
+                    record.clear()
+                title = line[1:]
+            else:
+                record.append(line)
+        if len(record) != 0:
+            join_str = ''.join(record)
+            if len(join_str.strip()) != 0:
+                yield title, join_str
+
+
+def write_fasta(records: Iterable[tuple[str, str]], filename: Path) -> Path:
+    # sequence is in one line, no truncated
+    if filename.exists():
+        log.warning(f'Overwriting existing file {filename}')
+    with open(filename, 'w') as f:
+        for title, seq in records:
+            f.write(f'>{title}\n')
+            f.write(f'{seq}\n')
+    return filename
+
+
+def fasta_to_array(records: Iterable[tuple[str, str]]) -> tuple[
+    np.ndarray, np.ndarray]:
+    records_ = list(records)
+    name_array = np.array([i[0] for i in records_], dtype=np.str_)
+    # S1 for 1 byte character, save more memory than 'U1' but
+    # encode/decode is required
+    seq_array = np.array([i[1] for i in records_], dtype=np.str_)
+    return name_array, seq_array
+
+
+def aln_to_array(records: Iterable[tuple[str, str]]) -> tuple[
+    np.ndarray, np.ndarray]:
+    records_ = list(records)
+    name_array = np.array([i[0] for i in records_], dtype=np.str_)
+    # S1 for 1 byte character, save more memory than 'U1' but
+    # encode/decode is required
+    seq_array = np.array(
+        [np.fromiter(i[1], dtype=np.dtype('S1')) for i in records_])
+    return name_array, seq_array
 
 
 def _compute_FN(mJ, n_cols: int, alphabet_size: int):
@@ -102,6 +179,70 @@ def main():
     run(str(align_file), verbose=False)
     return
 
+def parse_args():
+    arg = argparse.ArgumentParser()
+    arg.add_argument('input', help='aligned protein sequence fasta file')
+    arg.add_argument('-s', '-score', dest='score', default=0.75,
+                     help='coevolution score threshold')
+    return arg.parse_args()
 
+
+def main2():
+    arg = parse_args()
+    fasta = Path(arg.input).resolve()
+    assert fasta.exists()
+    result = fasta.with_suffix('.txt')
+    co = fasta.with_suffix('.co.aln')
+    non_co = fasta.with_suffix('.non_co.aln')
+    co_half = fasta.with_suffix('.co_half.aln')
+    non_co_half = fasta.with_suffix('.non_co_half.aln')
+    invariant = fasta.with_suffix('.invariant.aln')
+    all_except_half_co = fasta.with_suffix('.all_except_half_co.aln')
+
+    name, old_seq = aln_to_array(parse_fasta(fasta))
+    unique_counts = np.array(
+        [len(np.unique(old_seq[:, i])) for i in range(old_seq.shape[1])])
+    invariant_index = np.where(unique_counts == 1)[0]
+    invariant_site = old_seq[:, invariant_index]
+    # mutant_index = np.where(unique_counts > 1)[0]
+
+    fnr = call_julia(str(fasta))
+    # convert from numpy.void
+    np_array = np.array([list(i) for i in fnr.to_numpy()])
+    np.savetxt(result, np_array, fmt=['%d', '%d', '%.18e'])
+
+    # start with 1->0
+    np_array[:, :2] -= 1
+    # todo: how to set threshold?
+    # strong coupling
+    threshold = 0.75
+    np_array2 = np_array[np_array[:, 2] > threshold]
+    all_index = set(np.arange(0, old_seq.shape[1]))
+    co_index = set(np_array2[:, :2].flatten().astype(int))
+    co_half_index = set(np_array2[:, 0].flatten().astype(int))
+    non_co_index = all_index - co_index - set(invariant_index)
+    non_co_half_index = all_index - co_half_index - set(invariant_index)
+    all_except_half_index = all_index - co_half_index
+    # output
+    co_site = old_seq[:, list(co_index)]
+    non_co_site = old_seq[:, list(non_co_index)]
+    co_half_site = old_seq[:, list(co_half_index)]
+    non_co_half_site = old_seq[:, list(non_co_half_index)]
+    all_except_half_site = old_seq[:, list(all_except_half_index)]
+    print(old_seq.shape[1], 'columns\n',
+          np_array.shape[0], 'pairs\n',
+          np_array2.shape[0], 'pairs big score\n',
+          invariant_index.shape[0], 'invariant sites\n',
+          len(co_index), 'coevolution sites\n',
+          len(non_co_index), 'non-coevoled sites')
+    array_to_fasta(name, co_site, co)
+    array_to_fasta(name, non_co_site, non_co)
+    array_to_fasta(name, invariant_site, invariant)
+    array_to_fasta(name, co_half_site, co_half)
+    array_to_fasta(name, non_co_half_site, non_co_half)
+    array_to_fasta(name, all_except_half_site, all_except_half_co)
+    # print(result, co, non_co, co_half, non_co_half)
+    print('Done!')
+    return
 if __name__ == '__main__':
     main()
